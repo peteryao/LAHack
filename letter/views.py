dffc24f953ba638bb9313c639aa43ff712d8e65f
Peter Yao <peteryao916@gmail.com>
2014-04-13 09:34:47 -0700

    Updated city page to actually show real info.

--- letter/views.py
+++ letter/views.py
@@ -58,14 +58,14 @@ def letter_single_view(request, letter_pk):
     if (photo_size[0] / photo_size[1] > 1.8):
         return render(request, 'letter/small_picture_text.html', context)
 
-    if (photo_size[0] / photo_size[1] > 3):
+    if (photo_size[0] / photo_size[1] > 2):
         return redner(request, 'small_picture_text', context, html )
     return render(request, 'letter/half_and_half.html', context)
 
 def letter_city_view(request, city_info, filter_choice):
     context = {}
     city_new = Letter.objects.filter(name__contains=city_info)
-    print city_new
+    context['city_info'] = city_info
     context['recent_letters'] = city_new
     if(int(filter_choice) == 1):
         no_texts = []
@@ -80,6 +80,7 @@ def letter_city_view(request, city_info, filter_choice):
                 no_photo.append(letter)
         context['recent_letters'] = no_photo
     context['filter'] = int(filter_choice)
+    context['full_city'] = city_new[0].name
     return render(request, 'letter/city_view.html', context)
 
 def letter_submit(request):
